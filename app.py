f7a7b5becd75c366591305c63403ff0eedd9dffe
Yashraj Monani <yashrajm16802@gmail.com>
2025-12-27 15:43:16 +0530

    Added templates and updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import socket
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello! I was deployed via Jenkins to a Kubeadm Cluster!:v2"
+@app.route("/")
+def index():
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return render_template('index.html', hostname=host_name, ip=host_ip)
+    except:
+        return render_template('index.html', hostname="Unknown", ip="Unknown")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
